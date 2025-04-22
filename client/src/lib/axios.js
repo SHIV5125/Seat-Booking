6a6bc153f4d31fff676417f0ab8e58643e6e34ee
Shivam <shivamsahane5125@gmail.com>
2025-04-23 01:01:56 +0530

    updated backend link

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Axios instance with base URL
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //baseURL: "http://localhost:5000/api",
+  baseURL: "https://seat-booking-bliw.onrender.com/api",
 });
 
 // Attach token to every request if available
